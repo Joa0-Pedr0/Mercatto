80995b81dbb15b977dbdfe41ab8f48fbd33f685e
Joa0-Pedr0 <nogueiralimajoaopedro@gmail.com>
2025-11-24 20:20:17 -0300

    Feat(productMigrate): Adicionado o amount

--- database/migrations/2025_11_24_220457_products.php
+++ database/migrations/2025_11_24_220457_products.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->decimal('price', 8, 2);
+            $table->string('amount');
             $table->timestamps();
         });
     }
