8cc70a347057750015572147273f83cd6f63c9df
Joa0-Pedr0 <nogueiralimajoaopedro@gmail.com>
2025-11-25 20:03:24 -0300

    Feat(SupplierController): Feito o controller dos fornecedores

---
+++ app/Http/Controllers/SupplierController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Supplier;
+
+class SupplierController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $suppliers = Supplier::all();
+        return view('suppliers.index', compact('suppliers'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('suppliers.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        Supplier::create($request->all());
+
+        return redirect()->route('suppliers.index')->with('sucess', 'Fornecedor adicionado com sucesso!');
+    }
+
+
+    public function edit(Supplier $supplier)
+    {
+        return view('suppliers.edit', compact('supplier'));
+    }
+
+    public function update(Request $request, Supplier $supplier)
+    {
+        $supplier->update($request->all());
+        return redirect()->route('suppliers.index')->with('sucess', 'Fornecedor atualizada com sucesso!');
+    }
+
+    public function destroy(Supplier $supplier)
+    {
+        $supplier->delete();
+        return redirect()->route('suppliers.index')->with('sucess', 'Fornecedor excluído com sucesso!');
+    }
+}
