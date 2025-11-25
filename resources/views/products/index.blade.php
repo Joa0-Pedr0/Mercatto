f3700a37c58419423cb55a331aceaaab2a4e7f71
Joa0-Pedr0 <nogueiralimajoaopedro@gmail.com>
2025-11-25 20:34:59 -0300

    Feat(index): Adicionado um botão para ir pros fornecedores

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -17,6 +17,7 @@
 
     <div class="mb-4 flex gap-3">
         <a href="{{ route('products.create') }}" class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">Novo Produto</a>
+        <a href="{{ route('suppliers.index') }}" class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">Fornecedores</a>
     </div>
 
     <table class="w-full bg-white shadow rounded overflow-hidden">
