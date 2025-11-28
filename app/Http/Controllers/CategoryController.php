60e80c81c02e3e519be0f0bf3b3336caf8d5a1a0
Joa0-Pedr0 <nogueiralimajoaopedro@gmail.com>
2025-11-28 18:12:39 -0300

    Feat(CategoryController): Remover importação não utilizada de Illuminate\Http\Request

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Http\Requests\StoreCategoryRequest; 
 use App\Http\Requests\UpdateCategoryRequest;
-use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
