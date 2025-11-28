7f88d5ebf89505fcc4d21e5301cdeddf3e5004ce
Joa0-Pedr0 <nogueiralimajoaopedro@gmail.com>
2025-11-28 16:51:52 -0300

    Feat(CategoriesIndexView): Adicionada opção de navegação para a página de vendas

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -20,6 +20,7 @@
         <a href="{{ route('customers.index') }}" class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">Clientes</a>
         <a href="{{ route('products.index') }}" class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">Produtos</a>
         <a href="{{ route('suppliers.index') }}" class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">Fornecedores</a>
+        <a href="{{ route('sales.index') }}" class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">Vendas</a>
     </div>
 
     <table class="w-full bg-white shadow rounded overflow-hidden">
