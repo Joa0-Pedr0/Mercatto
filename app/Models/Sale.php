3c3eb4572b62e300d96fcb7fe1f2ad42d8653e62
Joa0-Pedr0 <nogueiralimajoaopedro@gmail.com>
2025-11-28 15:55:52 -0300

    Feat(Sale): Criado modelo de venda com relacionamentos para cliente e produto

---
+++ app/Models/Sale.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Sale extends Model
+{
+    protected $fillable = ['payment', 'customer_id', 'product_id'];
+
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+}
