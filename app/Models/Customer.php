4f59ebf7f8ef1e636db085c5a14d38665e2183f9
Joa0-Pedr0 <nogueiralimajoaopedro@gmail.com>
2025-11-28 15:55:21 -0300

    Feat(Customer): Adicionado relacionamento de vendas ao modelo de cliente

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Customer extends Model
 {
         protected $fillable = ['name', 'cpf', 'address'];
+        
+        public function sales(): HasMany
+    {
+        return $this->hasMany(Sale::class);
+    }
 }
