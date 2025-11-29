3f563d42542da4fe9775e87aa7e2823c3f7e81ed
Joa0-Pedr0 <nogueiralimajoaopedro@gmail.com>
2025-11-29 18:09:35 -0300

    Feat(ProductRequest): Adicionado as mensagens para a validação

--- app/Http/Requests/UpdateProductRequest.php
+++ app/Http/Requests/UpdateProductRequest.php
@@ -28,4 +28,25 @@ class UpdateProductRequest extends FormRequest
             'category_id' => ['required', 'exists:categories,id'],
         ];
     }
+    
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'O nome do produto é obrigatório.',
+            'name.max' => 'O nome do produto não pode exceder 255 caracteres.',
+
+            'amount.required' => 'A quantidade do produto é obrigatória.',
+            'amount.integer' => 'A quantidade do produto deve ser um número inteiro.',
+            'amount.min' => 'A quantidade do produto não pode ser negativa.',
+            
+            'price.required' => 'O preço do produto é obrigatório.',
+            'price.numeric' => 'O preço do produto deve ser um número.',
+            'price.min' => 'O preço do produto não pode ser negativo.',
+
+            'category_id.required' => 'A categoria do produto é obrigatória.',
+            'category_id.exists' => 'A categoria selecionada é inválida.',
+
+        ];
+
+    }
 }
